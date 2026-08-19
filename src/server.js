adc9479b238d958fe08f5418a48e18a2b1127c77
Maria Khomynets <masha.khomynets@gmail.com>
2026-08-19 11:12:53 +0300

    feat: add notFoundHandler middleware for handling 404 errors

--- src/server.js
+++ src/server.js
@@ -1,30 +1,17 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import pinoHttp from 'pino-http';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { Note } from './models/note.js';
 
 const app = express();
+app.use(logger);
 
 app.use(cors());
 app.use(express.json());
-app.use(
-  pinoHttp({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
 
 app.get('/notes', async (req, res) => {
   const notes = await Note.find();
@@ -46,17 +33,9 @@ app.get('/test-error', () => {
   throw new Error('Simulated server error');
 });
 
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
+app.use(notFoundHandler);
 
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: err.message,
-  });
-});
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 await connectMongoDB();
