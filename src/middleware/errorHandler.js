1e5ed911ba11b675ae9fc8b569a947177d011fb2
Maria Khomynets <masha.khomynets@gmail.com>
2026-08-19 12:40:20 +0300

    feat: implement CRUD operations for notes with error handling

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,12 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  console.error(err);
+  console.error('Error Middleware:', err);
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  }
 
   const isProd = process.env.NODE_ENV === 'production';
 
