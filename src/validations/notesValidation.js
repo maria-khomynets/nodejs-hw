1e2b063122d6a8968164ab567ece342f30b18c52
Maria Khomynets <masha.khomynets@gmail.com>
2026-08-24 15:38:13 +0300

    feat: add tags constant and validation schema for note creation

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,11 @@
+import { Joi, Segments } from 'celebrate';
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    age: Joi.number().integer().min(12).max(65).required(),
+    gender: Joi.string().valid('male', 'female', 'other').required(),
+    avgMark: Joi.number().min(2).max(12).required(),
+    onDuty: Joi.boolean(),
+  }),
+};
