ac7f5d803811c7e04b559c4a441d0ebd6308ab13
Maria Khomynets <masha.khomynets@gmail.com>
2026-09-04 14:18:55 +0300

    fix: clear cookies on expired refresh token in refreshUserSession

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -74,6 +74,14 @@ export const refreshUserSession = async (req, res, next) => {
 
     if (new Date() > session.refreshTokenValidUntil) {
       await Session.deleteOne({ _id: session._id });
+      const cookieOptions = {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      };
+      res.clearCookie('sessionId', cookieOptions);
+      res.clearCookie('accessToken', cookieOptions);
+      res.clearCookie('refreshToken', cookieOptions);
       throw createHttpError(401, 'Session token expired');
     }
 
